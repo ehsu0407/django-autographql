ed8b5ae08c3c49f496bf380c363904d2706612c7
Eddie Hsu <ehsu0407@gmail.com>
2022-03-09 18:28:59 -0500

    Adding missing enums.py

---
+++ autographql/filters/enums.py
@@ -0,0 +1,21 @@
+import graphene
+
+
+class WeekDay(graphene.Enum):
+    SUNDAY = 1
+    MONDAY = 2
+    TUESDAY = 3
+    WEDNESDAY = 4
+    THURSDAY = 5
+    FRIDAY = 6
+    SATURDAY = 7
+
+
+class IsoWeekDay(graphene.Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
